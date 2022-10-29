e20d40d851f37d745d20e4b36a1d6e38d71b915a
Aditya Srivastava <adityalpha@gmail.com>
2022-10-28 20:37:01 -0400

    adding new service and update triggers

--- admin.sql
+++ admin.sql
@@ -9,7 +9,6 @@ Input parameters
 - Add New Store - SID, ADDRESS, MIN_RATE, MIN_RATE,
 FIRSTNAME, LASTNAME, USERNAME, PASSWORD,
 SALARY
-- Add New Service - SERVICE_CAT, NAME, DURATION
 */
 
 
@@ -43,15 +42,52 @@ insert into EMPLOYEE (
 
 /* Triggers upon insertion to employee (if role = 'm') */
 
-insert into MECHANIC (
+CREATE TRIGGER ADD_MANAGER
+AFTER INSERT ON EMPLOYEES
+WHEN role = 'm'
+BEGIN
+insert into MANAGER (
 "eid",
 "salary") VALUES (
     EID,
     SALARY
-);
+)
+END;
 
+CREATE TRIGGER UPDATE_SC
+AFTER INSERT ON MANAGER
+BEGIN
 update SERVICE_CENTER
 set manager_id = EID
-where sid = SID;
+where sid = SID
+END;
 
 /* Triggers end here */
+
+/*
+Input parameters 
+- Add New Service - SERVICE_CAT, NAME, DURATION, NUMBER(?), MANF(?)
+*/
+
+/* Service  here is service category and schedule number */
+insert into SERVICES (
+"name",
+"number",
+"repair_cat") VALUES (
+    NAME,
+    NUMBER,
+    SERVICE_CAT
+);
+
+/* Triggers upon insertion to services */
+
+insert into DURATION_DETAILS (
+"manf",
+"duration",
+"name",
+"number") VALUES (
+    MANF,
+    DURATION,
+    NAME,
+    NUMBER
+);
